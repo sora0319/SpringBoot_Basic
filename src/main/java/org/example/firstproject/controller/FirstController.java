f91f97426f10eaa6ddf0bf53b2fcb53943d33860
sora0319 <sojeoung536985@gmail.com>
2024-08-05 12:48:08 +0900

    feat : add seeYouNext controller

---
+++ src/main/java/org/example/firstproject/controller/FirstController.java
@@ -0,0 +1,22 @@
+package org.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "사용자2");
+        return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("username", "사용자");
+        return "goodbye";
+    }
+    
+}
