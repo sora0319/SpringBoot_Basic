4542f8339c82bc92bcde033e629aac387a0adf98
sora0319 <sojeoung536985@gmail.com>
2024-08-06 00:29:07 +0900

    feat : make entity for db connect

---
+++ src/main/java/org/example/firstproject/entity/Article.java
@@ -0,0 +1,35 @@
+package org.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
