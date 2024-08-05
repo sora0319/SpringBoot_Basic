9c913a2d77797fceeb086d18e00b184407505139
sora0319 <sojeoung536985@gmail.com>
2024-08-06 00:31:08 +0900

    feat : save form data to entity in database

--- src/main/java/org/example/firstproject/controller/ArticleController.java
+++ src/main/java/org/example/firstproject/controller/ArticleController.java
@@ -1,12 +1,18 @@
 package org.example.firstproject.controller;
 
 import org.example.firstproject.dto.ArticleForm;
+import org.example.firstproject.entity.Article;
+import org.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm(){
@@ -16,6 +22,14 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
         System.out.println(form.toString());
+
+        // DTO를 엔티티로 변환
+        Article article = form.toEntity();
+        System.out.println(article.toString());
+
+        // repository 를 엔티티 db에 저장
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
         return "";
     }
 }
