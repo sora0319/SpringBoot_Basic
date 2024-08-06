5238530fdbb815c34e2d52f4ec4fec80d3ab04ae
sora0319 <sojeoung536985@gmail.com>
2024-08-06 13:32:30 +0900

    feat : apply redirect url in member mange page

--- src/main/java/org/example/firstproject/controller/MemberController.java
+++ src/main/java/org/example/firstproject/controller/MemberController.java
@@ -6,8 +6,12 @@ import org.example.firstproject.entity.Member;
 import org.example.firstproject.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -16,19 +20,36 @@ public class MemberController {
     @Autowired
     private MemberRepository memberRepository;
 
+    // 회원 가입 페이지
     @GetMapping("/signup")
     public String signUp() {
         return "members/new";
     }
 
+    // 폼 데이터 저장
     @PostMapping("/join")
     public String join(MemberForm memberForm) {
         Member member = memberForm.toEntity();
         Member saved = memberRepository.save(member);
         log.info(saved.toString());
 
-        return "";
+        return "redirect:/members/" + saved.getId();
     }
 
+    // 상세 페이지
+    @GetMapping("/members/{id}")
+    public String showDetail(@PathVariable("id") Long id, Model model) {
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", member);
+        return "members/show";
+    }
 
+    // 목록 페이지
+    @GetMapping("/members")
+    public String showIndex(Model model) {
+        List<Member> members = (List<Member>) memberRepository.findAll();
+        model.addAttribute("members", members);
+
+        return "members/index";
+    }
 }
