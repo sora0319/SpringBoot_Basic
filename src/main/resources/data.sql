dff0ebef1b167ad203a795b803e99fdb4857912e
sora0319 <sojeoung536985@gmail.com>
2024-08-06 22:55:46 +0900

    feat : initialize dummy data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO article(id, title, content)
+VALUES (1, 'aaaaa', '1234');
+
+INSERT INTO article(id, title, content)
+VALUES (2, 'bbbb', '4567');
+
+INSERT INTO article(id, title, content)
+VALUES (3, 'cccc', '8910');
