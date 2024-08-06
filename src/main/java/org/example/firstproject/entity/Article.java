d3a1df28bed587cb6aba56cec8795850cb714969
sora0319 <sojeoung536985@gmail.com>
2024-08-06 11:44:03 +0900

    feat : add getter to use lombok annotation

--- src/main/java/org/example/firstproject/entity/Article.java
+++ src/main/java/org/example/firstproject/entity/Article.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
+@Getter
 @AllArgsConstructor // 모든 매개변수가 들어간 생성자 추가 어노테이션
 @NoArgsConstructor // 기본 생성자 추가 어노테이션
 @ToString
