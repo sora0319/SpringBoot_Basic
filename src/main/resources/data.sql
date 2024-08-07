9a77695c37ed8b3b1c024f24bc3893598bde8063
sora0319 <sojeoung536985@gmail.com>
2024-08-07 10:38:16 +0900

    feat : change query setting

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO article(id, title, content)
-VALUES (1, 'aaaaa', '1234');
+INSERT INTO article(title, content)
+VALUES ('aaaaa', '1234');
 
-INSERT INTO article(id, title, content)
-VALUES (2, 'bbbb', '4567');
+INSERT INTO article(title, content)
+VALUES ('bbbb', '4567');
 
-INSERT INTO article(id, title, content)
-VALUES (3, 'cccc', '8910');
+INSERT INTO article(title, content)
+VALUES ('cccc', '8910');
